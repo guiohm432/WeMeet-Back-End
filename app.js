66cbc9c98a7683eef9e8ffc76987af9a766a3c06
Guillaume Marcellin <marcellinguillaume@hotmail.fr>
2020-10-20 13:31:12 +0200

    SET UP MODIFICATIONS dquote> Erase useless views dquote> Create new dev script with nodemon Create index.html in public folder to test rendering and PORT Comment unnecessary code for now in app.js

--- app.js
+++ app.js
@@ -1,41 +1,62 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require("dotenv").config();
+// require("./config/dbConnection");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const session = require("express-session");
+// const MongoStore = require("connect-mongo")(session);
+// const mongoose = require("mongoose");
+const app = express();
+const cors = require("cors");
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+/**
+ * Middlewares
+ */
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+  credentials: true
+};
+app.use(cors(corsOptions));
+app.use(logger("dev")); // This logs HTTP reponses in the console.
+app.use(express.json()); // Access data sent as json @req.body
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+// app.use(
+//   session({
+//     store: new MongoStore({
+//       mongooseConnection: mongoose.connection
+//     }),
+//     secret: process.env.SESSION_SECRET,
+//     resave: true,
+//     saveUninitialized: true,
+//   })
+// );
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Test to see if user is logged In before getting into any router.
+// app.use(function (req, res, next) {
+//   console.log(req.session.currentUser);
+//   next();
+// });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+/**
+ * Routes
+ */
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+const indexRouter = require("./routes/index");
+// const authRouter = require("./routes/auth");
+// const projectRouter = require("./routes/project");
+// const tagRouter = require("./routes/tag");
+// const profileRouter = require("./routes/profile");
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use("/", indexRouter);
+// app.use("/api/auth", authRouter);
+// app.use("/api/projects", projectRouter);
+// app.use("/api/tags", tagRouter);
+// app.use("/api", profileRouter);
 
 module.exports = app;
